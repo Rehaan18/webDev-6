1a974579f3928399622de5ca06731c634514a43d
Rehaan18 <mdrehaanb8@gmail.com>
2026-04-29 19:27:46 +0530

    js done

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -1,7 +1,6 @@
 console.log("Hello WOrld");
-var a = 5;
+
 var b ="Hello";
-let x = 2;
 
 const pi = 3.14 ;
 console.log("Task Complete");
@@ -10,3 +9,29 @@ let x = 10;
 let y = "10";
 
 console.log(x==y);
+
+for(var i=1;i<=10;i++)
+{
+    console.log("2xi="+i*2);
+}
+
+
+let a = [24, 58, 33, 86, 96, 12, 53, 23, 32, 100];
+// for (let i = 0; i < a.length; i++) 
+// {
+//   console.log(a[i]); 
+// }
+// let j =0;
+// while(j<10)
+// {
+//     console.log(a[j]);
+//     j++;
+// } 
+
+let j=0;
+do
+{
+    console.log(a[j]);
+    j++;
+}
+while(j<10);
