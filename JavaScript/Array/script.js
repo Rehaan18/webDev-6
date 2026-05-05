b9a8c845173b3298cba588ba541a6d0dc2c25498
Rehaan18 <mdrehaanb8@gmail.com>
2026-05-05 19:45:04 +0530

    Array and its application

---
+++ JavaScript/Array/script.js
@@ -0,0 +1,53 @@
+// let arr = [1, 2, 3, 4, 5, 6, 7, 8]; //creation of Array
+
+// for (
+//   var i = 0;
+//   i < 8;
+//   i++ //Reading of array
+// ) {
+//   console.log(arr[i]);
+// }
+
+// console.log(arr);
+
+// arr[2] = arr[2] + 10; //Updation of array
+// console.log(arr);
+
+// arr = [];
+// console.log(arr); //deletion of array
+
+// hence the CRUD operation has been done since we have perfomed each task of Creation and Reading and Updation and Deletion
+
+//all the below comes under the U of CRUD
+let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+console.log(arr);
+
+arr.push(9);
+console.log(arr);
+
+arr.pop();
+console.log(arr);
+
+arr.unshift(1);
+console.log(arr);
+
+arr.shift(1);
+console.log(arr);
+
+console.log(arr.at(0));
+
+console.log(arr.indexOf(5));
+
+console.log(arr.includes(60));
+console.log(arr.indexOf(60));
+
+let array = [23, 5, 56, 758, 89, 34, 23];
+console.log(array);
+array.sort();
+console.log(array);
+
+array.reverse();
+
+console.log(array)
+
+
