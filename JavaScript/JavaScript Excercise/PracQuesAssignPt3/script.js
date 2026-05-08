61e4784496177fdb7afe4e9a99e428b1de332ee8
Rehaan18 <mdrehaanb8@gmail.com>
2026-05-08 18:53:49 +0530

    Javascript exercise pt 3 4 questions done

---
+++ JavaScript/JavaScript Excercise/PracQuesAssignPt3/script.js
@@ -0,0 +1,18 @@
+//1
+let a = 23;
+let b = 43;
+console.log("a + b =",a+b);
+
+
+//2
+console.log("Remainder = ",a/b)
+
+//3
+console.log(2 ** 3);
+
+//4
+let x = 24;
+x += 23;
+console.log(x);
+x -= 12;
+console.log(x);
