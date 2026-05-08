96c26998c89b03fa5e5e79ebac87e5a189c63e2b
Rehaan18 <mdrehaanb8@gmail.com>
2026-05-08 12:14:21 +0530

    practiseSheet 1 and 2 done

---
+++ JavaScript/JavaScript Excercise/PracQuesAssignPt2/script.js
@@ -0,0 +1,60 @@
+//1
+
+let name;
+let age;
+let StudentsStat;
+
+
+//2
+var name2;
+let hue;
+const right = "hello";
+
+//3
+let name1;
+
+name1 = "Rehaan";
+console.log(name1)
+
+//4
+let a = "abc";
+let b = 2;
+let c = true;
+
+//5
+let firstVar;
+let secondVar = null;
+
+//6
+let g = 5;
+console.log(typeof(g));
+
+//7
+var e = 34;
+var o = "Hello";
+
+console.log(e,o);
+
+
+//8
+let dig1 = 48;
+let dig2 = 23;
+console.log("a + b = " , dig1 + dig2);
+
+//9
+let student =
+{
+    name : "Rehaan" ,
+    age : 19
+}
+console.log(student.name);
+
+//10
+let x = "Hello"
+x = 24;
+console.log(x)
+
+//11
+const  y= 24;
+y = "wda";
+console.log(y)
