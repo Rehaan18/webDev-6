57f849cb1e992be22eb1c93fe2b8e83472f3a00f
Rehaan18 <mdrehaanb8@gmail.com>
2026-05-23 20:52:51 +0530

    yayyyy api ka use kiya yayyy

---
+++ JavaScript/API/Project Using API/script.js
@@ -0,0 +1,20 @@
+async function GetNewJoke()
+{
+    const API_URL = "https://official-joke-api.appspot.com/jokes/random"
+    const response = await fetch(API_URL);
+    const data = await response.json();
+    // if(data.type === "single")
+    // {
+    //     document.getElementById("setup").innerText = data.joke
+    //     document.getElementById("delivery").innerText = "";
+    // }
+    // else
+    // {
+    //     document.getElementById("setup").innerText = data.setup
+    //     document.getElementById("delivery").innerText = data.delivery
+    // }
+    //another method
+    document.getElementById("setup").innerText = data.joke || data.setup;
+    document.getElementById("delivery").innerText = data.punchline ||  "";
+
+ }
