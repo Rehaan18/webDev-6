ac9949a031efca7f1923415104e6e2b3b02b580a
Rehaan18 <mdrehaanb8@gmail.com>
2026-05-25 20:39:42 +0530

    multiple api projects done

---
+++ JavaScript/API/Currency Calculator/script.js
@@ -0,0 +1,16 @@
+async function convertCurrency() {
+  const amount = document.getElementById("amount").value;
+  const fromCurrency = document.getElementById("fromCurrency").value;
+  const toCurrency = document.getElementById("toCurrency").value;
+
+  const url = `https://api.exchangerate-api.com/v4/latest/${fromCurrency}`;
+
+  const response = await fetch(url);
+  const data = await response.json();
+
+  const rate = data.rates[toCurrency];
+  const convertedAmount = (amount * rate).toFixed(2);
+
+  document.getElementById("result").innerText =
+    `Result : ${amount} ${fromCurrency} = ${convertedAmount} ${toCurrency}`;
+}
---
+++ JavaScript/API/Weather App/script.js
@@ -0,0 +1,33 @@
+const API_KEY = "f68cced823131fd27b6a3e53c987e0f6";
+
+async function getWeather() {
+
+    const cityName = document.getElementById("cityName").value.trim();
+    const { latitude, longitude } = await getGeoLocation(cityName);
+    const WEATHER_API =
+        `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${API_KEY}`;
+
+    const response = await fetch(WEATHER_API);
+    const data = await response.json();
+    console.log(data);
+    const temperature = data.main.temp - 273.15;
+    document.getElementById("temp").innerText =
+        temperature.toFixed(2) + " °C";
+}
+
+async function getGeoLocation(city) {
+
+    const GEO_LOC_API =
+        `https://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=1&appid=${API_KEY}`;
+
+    const response = await fetch(GEO_LOC_API);
+
+    const data = await response.json();
+
+    console.log(data);
+
+    const latitude = data[0].lat;
+    const longitude = data[0].lon;
+
+    return { latitude, longitude };
+}
