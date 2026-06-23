f7a3dae79c6d8a31a67805e29bec62952aaa39f1
Rehaan18 <mdrehaanb8@gmail.com>
2026-06-23 19:02:40 +0530

    work of controller and routers

--- Backend/firstBackend/index.js
+++ Backend/firstBackend/index.js
@@ -2,31 +2,22 @@ import dotenv from "dotenv";
 dotenv.config();
 
 import express from "express";
+import AuthRouter from "./src/routers/auth.route.js";
+import PublicRouter from "./src/routers/public.route.js";
 
 const app = express();
 
+app.use("/auth", AuthRouter);
+app.use("/public", PublicRouter);
+
+//Default API
 app.get("/", (req, res) => {
-  console.log("Default Get API hit");
-  res.json({ message: "Welcome to my first backend" });
-});
-
-app.post("/login", (req, res) => {
-  console.log("Login Successful");
-  res.json({ message: "Login Succesfull" });
-});
-
-app.post("/register", (req, res) => {
-  console.log("Registeration Done");
-  res.json({ message: "Register Successfull" });
-});
-
-app.get("/loginanimation", (req, res) => {
-  console.log("Welcome to login");
-  res.json({ message: "Login Page Animation Called" });
+  console.log("Default Get API Hit");
+  res.json({ message: "Welcome to my first backend Project" });
 });
 
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => {
-  console.log("Server started on port: ", port);
+  console.log("Server Started on port:", port);
 });
